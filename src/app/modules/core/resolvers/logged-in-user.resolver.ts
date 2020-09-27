c62a59ece81d92c4bebee01084bf8989a2e905bc
Harsh Mittal <mittalh863@gmail.com>
2020-09-28 00:23:30 +0530

    chnages in logged in user resolver

--- src/app/modules/core/resolvers/logged-in-user.resolver.ts
+++ src/app/modules/core/resolvers/logged-in-user.resolver.ts
@@ -1,5 +1,4 @@
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
-import {Observable} from 'rxjs';
 import {LoggedInUserManager} from '../managers/logged-in-user.manager';
 import {UserModel} from '../../shared/models/user.model';
 import {take} from 'rxjs/operators';
@@ -10,8 +9,8 @@ export class LoggedInUserResolver implements Resolve<UserModel>{
   constructor(private _loggedInUserManager: LoggedInUserManager) {
   }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<UserModel> | Promise<UserModel> | UserModel {
-    return this._loggedInUserManager.selectLoggedInUser().pipe(take(1));
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<UserModel> {
+    return this._loggedInUserManager.selectLoggedInUser().pipe(take(1)).toPromise();
   }
 
 }
