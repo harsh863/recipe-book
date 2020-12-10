59b57453bbcf72e4a2337a2b6328b99714f29f40
Harsh Mittal <mittalh863@gmail.com>
2020-12-10 10:52:29 +0530

    prevented app component called twice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from './app.component';
 import {AuthGuard} from './guards/auth.guard';
 import {LoggedInUserResolver} from './modules/core/resolvers/logged-in-user.resolver';
 import {PageNotFoundComponent} from './modules/shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
-    path: '', component: AppComponent, children: [
-      {
-        path: 'dashboard',
-        loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
-        canActivate: [AuthGuard], resolve: [LoggedInUserResolver]
-      },
-      {
-        path: 'auth',
-        loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
-      },
-      {
-        path: '', redirectTo: 'dashboard', pathMatch: 'full'
-      },
-      {
-        path: '**', component: PageNotFoundComponent
-      }
-    ]
+    path: 'dashboard',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivate: [AuthGuard], resolve: [LoggedInUserResolver]
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: '', redirectTo: 'dashboard', pathMatch: 'full'
+  },
+  {
+    path: '**', component: PageNotFoundComponent
   }
 ];
 
